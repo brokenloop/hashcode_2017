df2d8d04eebfedafad9261b559277e7009418ac1
Daniel Jordan <daniel.jordan1@ucdconnect.ie>
2017-04-04 14:14:22 +0100

    updated hill climb, added change scorer

--- main.py
+++ main.py
@@ -41,6 +41,46 @@ def score(solution, data):
     return (time_saved * 1000) // total_requests
 
 
+
+def score_change(video, cache, data):
+
+    if video not in data["video_ed_request"]:
+        return 0
+
+    # time taken to stream all videos from datacentre
+    time_saved = 0
+
+    for endpoint in data["video_ed_request"][video]:
+
+        num_of_requests = data["video_ed_request"][video][endpoint][0]
+
+        # list of caches connected to an endpoint
+        connections = data["ed_cache_list"][endpoint]
+
+
+        if cache in connections:
+            current_smallest = data["video_ed_request"][video][endpoint][1]
+            cache_lat = data["ep_to_cache_latency"][endpoint][cache]
+            if cache_lat < current_smallest:
+                time_saved += (current_smallest - cache_lat) * num_of_requests
+
+    return time_saved
+
+
+def update_min_latency(video, cache, data):
+
+    for endpoint in data["video_ed_request"][video]:
+
+        # list of caches connected to an endpoint
+        connections = data["ed_cache_list"][endpoint]
+
+        if cache in connections:
+            current_smallest = data["video_ed_request"][video][endpoint][1]
+            cache_lat = data["ep_to_cache_latency"][endpoint][cache]
+            if cache_lat < current_smallest:
+                data["video_ed_request"][video][endpoint][1] = cache_lat
+
+
 def generate_solution(data):
     grid = []
     cache_contents = {}
@@ -60,6 +100,19 @@ def generate_solution(data):
     return grid, cache_contents
 
 
+def generate_empty(data):
+    grid = []
+    cache_contents = {}
+
+    for i in range(data["number_of_caches"]):
+        cache_contents[i] = 0
+        row = []
+        for j in range(data["number_of_videos"]):
+            row.append(0)
+        grid.append(row)
+
+    return grid, cache_contents
+
 
 def check_validity(solution, data):
 
@@ -76,57 +129,49 @@ def check_validity(solution, data):
     else:
         return True
 
-
-def score_change(solution, video, cache, cache_contents, data):
-
-    # time taken to stream all videos from datacentre
-    time_saved = 0
-    total_requests = 0
-
-    # form of request is as follows: (video, endpoint): num_of_requests
-    for i in range(data["number_of_endpoints"]):
-        key = (str(video), str(i))
-        if key in data["video_ed_request"]:
-            endpoint = i
-            num_of_requests = int(data["video_ed_request"][key])
-
-            dc_latency = data["ep_to_dc_latency"][endpoint]
-
-            # list of caches connected to an endpoint
-            connections = data["ed_cache_list"][endpoint]
-            # list of latencies of caches with video
-            caches = [dc_latency,]
-
-            # find lowest latency cache with video.
-            # if ep and cache have no connection, nothing happens
-            for cache in connections:
-                if solution[cache][video] == 1:
-                    caches.append(data["ep_to_cache_latency"][endpoint][cache])
-
-            time_saved += ((dc_latency - min(caches)) * num_of_requests)
-            total_requests += num_of_requests
-
-    return (time_saved * 1000) // total_requests
+def valid_move(sol, cache, video, cache_contents, data):
+    if sol[cache][video] == 1:
+        return False
+    return (cache_contents[cache] + data["video_size_desc"][video] <= data["cache_size"])
 
 
+def hill_climb(sol, cache_contents, data):
 
-def hill_climb(solution, contents, data):"""
-    highest = 0
-    for cache in caches:
-        for video in videos:
-            check validity of change (using contents)
-            check score of change
-            if score is higher than highest:
-                record change
-                change score
-    """
+    move_found = True
+    while move_found:
+        best_score = 0
+        best_move = []
+        move_found = False
 
+        for i in range(len(sol)):
+            for j in range(len(sol[0])):
+                if valid_move(sol, i, j, cache_contents, data):
+
+                    # have to order these backwards because of how the scoring function works
+                    newscore = score_change(j, i, data)
+                    if newscore > best_score:
+                        best_score = newscore
+                        best_move = [i, j]
+                        move_found = True
+
+        if move_found:
+            print("Best move is", best_move, "with score:", best_score)
+            cache, video = best_move[0], best_move[1]
+            sol[cache][video] = 1
+            cache_contents[cache] += data["video_size_desc"][video]
+
+            # need to do this to reset the minimum latency for each request.
+            # test which one is faster
+            # results: It doesn't make much of a difference. update_min_latency is about 5-10% faster.
+            update_min_latency(video, cache, data)
+            # score(sol, data)
 
 
 
 if __name__=="__main__":
 
-    data = read_google("input/me_at_the_zoo.in")
+    data = read_google("input/videos_worth_spreading.in")
+    # me_at_the_zoo = 516740
 
 
     # sample solution given in hashcode brief - works for example.in
@@ -134,7 +179,11 @@ if __name__=="__main__":
                 [0, 1, 0, 1, 0],
                 [1, 1, 0, 0, 0]]
 
+    print("test")
     newsol, newsol_contents = generate_solution(data)
-    print(score(newsol, data))
+    print("original score:", score(newsol, data))
+    # hill_climb(newsol, newsol_contents, data)
+    # print("final score:", score(newsol, data))
+
 
 
